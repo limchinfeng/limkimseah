03ec3dc27cfe73df11c21046745ebba8c3289891
supershuaifeng <limchinfeng@gmail.com>
2023-08-14 16:15:20 +0800

    edit meta details

--- src/pages/profile.js
+++ src/pages/profile.js
@@ -15,7 +15,7 @@ const profile = () => {
           ISAT is a spinal cord correction technique that uses an activator to apply precise force to the spine, realigning the spinal joints and clearing the nerves, thereby improving pain and discomfort in various parts of the body."
         />
         <meta name="viewport" content="width=device-width, initial-scale=1" />
-        <link rel="icon" href="/favicon.ico" />
+        <link rel="icon" href="favicon.ico" />
       </Head>
       <main>
         <Layout>
